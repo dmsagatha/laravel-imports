8724f18a70cf860cba9f9e913c9c338a11643c72
Doris Morales <dmorales55@gmail>
2022-09-09 16:04:16 -0500

    Laravel: Importar datos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
 import './bootstrap';
 
-import '@fortawesome/fontawesome-free/js/all.js';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/laravelMix', function () {
-  return view('layouts.app');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
