2193508e50c5ca7e851d5bc54d734bc5813ee89e
Doris Morales <dmorales55@gmail>
2022-09-09 16:29:57 -0500

    Importar datos y elegir los campos coincidentes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Imports\ImportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,5 +10,16 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+/**
+ * https://github.com/LaravelDaily/Laravel-8-Import-CSV
+ * Revisar: Laravel Excel: Import with Relationships
+ * https://www.youtube.com/watch?v=n2WOag1G7Zg
+ */
+Route::controller(ImportController::class)->group(function () {
+  Route::get('/daily-usuarios', 'getImport')->name('import');
+  Route::post('/import_parse', 'parseImport')->name('import_parse');
+  Route::post('/import_process', 'processImport')->name('import_process');
+});
 
 require __DIR__.'/auth.php';
